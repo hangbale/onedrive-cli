5c45564635277c9e7940cfa2d945eccdfbf68ddf
leihang <leihang@mchz.com.cn>
2023-11-07 17:33:03 +0800

    兼容目录

--- src/config.rs
+++ src/config.rs
@@ -18,7 +18,31 @@ pub struct OnedriveConfig {
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Config {
     pub onedrive: OnedriveConfig,
-    pub files: Vec<String>
+    pub files: Vec<String>,
+    pub parsed_files: Option<Vec<String>>
+}
+
+fn is_dir(path: &str) -> bool {
+    let path = Path::new(path);
+    path.is_dir()
+}
+
+fn traverse_dir(path: &str) -> Vec<String> {
+    let mut files: Vec<String> = Vec::new();
+    if is_dir(path) {
+        for entry in std::fs::read_dir(path).unwrap() {
+            let entry = entry.unwrap();
+            let path = entry.path();
+            if path.is_dir() {
+                files.append(&mut traverse_dir(path.to_str().unwrap()));
+            } else {
+                files.push(path.to_str().unwrap().to_string());
+            }
+        }
+    } else {
+        files.push(path.to_string());
+    }
+    files
 }
 
 pub fn get_config() -> Config {
@@ -28,7 +52,12 @@ pub fn get_config() -> Config {
     } else {
         let config_file = File::open(CONFIG_FILE_PATH).unwrap();
         match serde_yaml::from_reader(config_file) {
-            Ok(config @ Config { .. }) => {
+            Ok(mut config @ Config { .. }) => {
+                let mut files: Vec<String> = Vec::new();
+                for file in &config.files {
+                    files.append(&mut traverse_dir(&file));
+                }
+                config.parsed_files = Some(files);
                 config
             }
             Err(e) => {
